96bda68cf1ac1e37a97c86b1062ee2694e41d708
zedali-dfe <zed.ali@education.gov.uk>
2024-03-06 13:31:11 +0000

    added session based intelligence

--- app/routes.js
+++ app/routes.js
@@ -19,41 +19,38 @@ var arrUsers = [
     {
         firstName: "Sheena",
         lastName: "Newman",
-        emailAddress: "sheena@somecouncil.org.uk",
+        emailAddress: "sheena@rbcouncil.org.uk",
         type: 'nqsw',
         regNumber: "SW2478"
     },{
         firstName: "Joe",
         lastName: "Blogg",
-        emailAddress: "joe@somecouncil.org.uk",
+        emailAddress: "joe@rbcouncil.org.uk",
         type: 'coordinator',        
     },{
         firstName: "Laura",
         lastName: "Barn",
-        emailAddress: "laura@somecouncil.org.uk",
+        emailAddress: "laura@rbcouncil.org.uk",
         type: 'nqsw',
-        status: "Missing registration number",
-        reason: "You have not provided a Social Work England registration number for this account"
     },{
         firstName: "Ricardo",
         lastName: "Athanasopoulos",
-        emailAddress: "ricardo@somecouncil.org.uk",
+        emailAddress: "ricardo@rbcouncil.org.uk",
         type: 'assessorCoordinator',
     },{
         firstName: "Yavuz",
         lastName: "Karci",
-        emailAddress: "Yavuz@somecouncil.org.uk",
+        emailAddress: "Yavuz@rbcouncil.org.uk",
         type: 'nqsw',
-        status: "Not linked",
-        regNumber: "SW142",
-        reason: "This account is no longer linked to this organisation"
+        status: "not_linked",
+        regNumber: "SW142"
     }
 ]
 
-sortUsers();
 
 router.get('/start', function(req, res) {
     req.session.version = req.query.version;
+    req.session.users = arrUsers;
     res.render('start');
 });
 
@@ -67,13 +64,24 @@ router.post('/one-login', function(req, res) {
 
 
 
+
+
 router.get('/account-creation-org-admin', function(req, res) {
 
-    var show_notification = undefined;
-    if (req.query.success)
-        show_notification = req.query.success;
+    
 
-    res.render('account-creation-org-admin/index', {show_notification: show_notification, users: arrUsers, user_types: oUserTypes});
+    var show_notification = undefined;
+    var user = undefined;
+    var users = req.session.users;
+
+    if (req.query.show_notification) {
+        show_notification = req.query.show_notification;
+        user = users[req.query.user_id];
+    }
+
+    sortUsers(req);
+
+    res.render('account-creation-org-admin/index', {show_notification: show_notification, users: users, user: user, user_types: oUserTypes});
 });
 
 router.get('/account-creation-org-admin/select-user-type', function(req, res) {
@@ -81,7 +89,7 @@ router.get('/account-creation-org-admin/select-user-type', function(req, res) {
     var user = undefined;
     if (req.query.editing){
         id = req.query.id;
-        user = arrUsers[id];
+        user = req.session.users[id];
     }
         
     res.render('account-creation-org-admin/select-user-type', {id: id, user: user, user_types: oUserTypes});
@@ -91,42 +99,48 @@ router.post('/account-creation-org-admin/select-user-type', function(req, res) {
     if (req.body.usertype == undefined)
         res.redirect('/account-creation-org-admin/select-user-type')
 
-    if (req.body.id)
+
+    if (req.body.id){
+        var id = req.body.id;
+        var user = req.session.users[id];
+        user.type = req.body.usertype;
+
         res.redirect(`/account-creation-org-admin/update-user?id=${req.body.id}&success=true`);
-    else
-        res.redirect('/account-creation-org-admin/add-user/?usertype='+req.body.usertype)
+    } else {
+        var user = {};
+        user.type = req.body.usertype;
+        user.new_account = true;
+        req.session.users.push(user);
+        id = req.session.users.length-1;
+
+        res.redirect(`/account-creation-org-admin/add-user/?id=${id}`);
+    }
 });
 
 router.get('/account-creation-org-admin/add-user', function(req, res) {
-    var id = undefined;
-    var user = undefined;
-
-    if(req.query.usertype)
-        user_type = req.query.usertype; 
-
-    if (req.query.editing){
-        id = req.query.id;
-        user = arrUsers[id];
-        user_type = user.type
-    }
-
-
+    var id = req.query.id;
+    var user = req.session.users[id];
+    user_type = user.type
+   
 
     res.render('account-creation-org-admin/add-user', {id: id, user: user, user_type: user_type});
 });
 
 router.post('/account-creation-org-admin/add-user', function(req, res) {
-    if (req.body.id) {
-        res.redirect('/account-creation-org-admin/update-user?id='+req.body.id);
-    } else {
-        var fname = req.body.firstName;
-        var lname = req.body.lastName;
-        var email = req.body.emailAdress;
-        var type = req.body.userType;
-        var reg_number = req.body.regnumber;
-         console.log(req.body)
 
-        res.redirect('/account-creation-org-admin/?success=user_added');
+    var user = req.session.users[req.body.id];
+
+    user.firstName = req.body.firstName;
+    user.lastName = req.body.lastName;
+    user.emailAddress = req.body.emailAddress;
+    if (req.body.regNumber != '')
+        user.regNumber = req.body.regNumber;
+    
+    if (user.new_account == true) {
+        user.new_account = false;
+        res.redirect(`/account-creation-org-admin/?show_notification=user_added&user_id=${req.body.id}`);
+    } else {
+        res.redirect(`/account-creation-org-admin/update-user?id=${req.body.id}`);
     }
 });
 
@@ -138,16 +152,51 @@ router.get('/account-creation-org-admin/update-user', function(req, res) {
     if (req.query.success)
         show_notification = req.query.success;
 
-    res.render('account-creation-org-admin/update-user', {id:id, user: arrUsers[id], user_types: oUserTypes, show_notification: show_notification});
+    res.render('account-creation-org-admin/update-user', {id:id, user: req.session.users[id], user_types: oUserTypes, show_notification: show_notification});
 });
 
+router.get('/link-user', function(req, res) {
+    var id = req.query.id;
+    req.session.users[id].status = undefined;
 
-function sortUsers(){
-    arrUsers.sort(function (a, b) {
-        if (a.fname < b.fname) {
+    res.redirect('/account-creation-org-admin/?success=associated');
+});
+
+router.get('/unlink-user', function(req, res) {
+    var id = req.query.id;
+    req.session.users[id].status = "not_linked";
+
+    res.redirect('/account-creation-org-admin/?success=dissociated');
+});
+
+router.get('/deactivate-account', function(req, res) {
+    var id = req.query.id;
+    var accountToDeactivate = req.session.users[id];
+    if(accountToDeactivate.type == "coordinator" || accountToDeactivate.type == "assessorCoordinator"){
+        var countCoordinators = 0;
+        req.session.users.forEach(user => {
+            if (user.status != "deactivated" && (user.type == "coordinator" || user.type == "assessorCoordinator"))
+                countCoordinators++;
+        });
+
+        if(countCoordinators == 1){
+            res.redirect('/account-creation-org-admin/cannot-deactivate');
+        } else {
+            req.session.users[id].status = "deactivated";
+            res.redirect(`/account-creation-org-admin/?show_notification=user_deactivated&id=${id}`);
+        }
+    }
+
+
+    // res.redirect('/account-creation-org-admin/?success=dissociated');
+});
+
+function sortUsers(req){
+    req.session.users.sort(function (a, b) {
+        if (a.firstName < b.firstName) {
             return -1;
         }
-        if (a.fname > b.fname) {
+        if (a.firstName > b.firstName) {
             return 1;
         }
         return 0;
